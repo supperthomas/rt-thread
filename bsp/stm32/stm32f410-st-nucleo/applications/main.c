55e8f8baf68608bb0274ce6d8be66ce7b936a8c9
supperthomas <78900636@qq.com>
2021-09-26 21:31:56 +0800

    remove the main

--- bsp/stm32/stm32f410-st-nucleo/applications/main.c
+++ bsp/stm32/stm32f410-st-nucleo/applications/main.c
@@ -15,8 +15,9 @@
 /* defined the LED0 pin: PA5 */
 #define LED0_PIN               GET_PIN(A, 5)
 
-int main(void)
+void main_thread_entry(void *parameter)
 {
+      
     __HAL_RCC_GPIOA_CLK_ENABLE();
     /* set LED0 pin mode to output */
     //rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
@@ -29,4 +30,60 @@ int main(void)
         GPIOA->BSRR = (1<<5)<<16;
         rt_thread_mdelay(500);
     }
+}
+/**
+ * @brief  This function will create and start the main thread, but this thread
+ *         will not run until the scheduler starts.
+ */
+static struct rt_thread _main_thread;
+static rt_uint8_t _main_thread_stack[512];
+void rt_application_init(void)
+{
+    rt_thread_t tid;
+
+//    tid = rt_thread_init("main", main_thread_entry, RT_NULL,
+//                           RT_MAIN_THREAD_STACK_SIZE, RT_MAIN_THREAD_PRIORITY, 20);
+//    
+       rt_thread_init(&_main_thread,
+                   "main",
+                   main_thread_entry,
+                   RT_NULL,
+                   &_main_thread_stack[0],
+                   sizeof(_main_thread_stack),
+                   2,
+                   10);
+
+    rt_thread_startup(&_main_thread);
+}
+
+void rtthread_startup(void)
+{
+    /* init board */
+    rt_hw_board_init();
+
+    /* init scheduler system */
+    rt_system_scheduler_init();
+        /* init application */
+    rt_application_init();
+
+    /* init timer thread */
+    rt_system_timer_thread_init();
+
+    /* init idle thread */
+    rt_thread_idle_init();
+
+    /* start scheduler */
+    rt_system_scheduler_start();
+
+    /* never reach here */
+    return ;
+}
+
+int main(void)
+{
+    rt_hw_interrupt_disable();
+
+    /* startup RT-Thread RTOS */
+    rtthread_startup();
+  
 }
