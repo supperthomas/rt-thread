c70c2b11a6dcdd43498d147f8028da9cf3260105
supperthomas <78900636@qq.com>
2022-09-17 09:05:07 +0800

    test

--- bsp/stm32/stm32l496-st-nucleo/applications/main.c
+++ bsp/stm32/stm32l496-st-nucleo/applications/main.c
@@ -23,8 +23,8 @@ int main(void)
     while (1)
     {
         rt_pin_write(LED2_PIN, PIN_HIGH);
-        rt_thread_mdelay(500);
+        rt_thread_mdelay(1000);
         rt_pin_write(LED2_PIN, PIN_LOW);
-        rt_thread_mdelay(500);
+        rt_thread_mdelay(1000);
     }
 }
